17d9a7ac4ea365a47ad8a2f6a543823a89d767f2
Marcelo Apala <cam5000810@est.univalle.edu>
2022-10-06 15:31:23 -0400

    Element Route,Controller

--- src/controllers/element.controllers.js
+++ src/controllers/element.controllers.js
@@ -1,1 +1,15 @@
 //ASIGNADO A MARCELO CONDORI
+import { pool } from "../db.js"
+
+export const getElement = (req,res) => res.send('Obtener elementos')
+
+export const createElement = async (req,res) => {
+    const {name,description,price,stock,category} = req.body
+    const [rows] = await pool.promise().query("INSERT INTO element(name,serialNumber,amount,description,unitOfMeasurement) VALUES (?,?,?,?,?)",
+    [name,serialNumber,amount,description,unitOfMeasurement])
+    res.send({rows})
+}
+
+export const updateElement = (req,res) => res.send('Obtener elementos')
+
+export const deleteElement = (req,res) => res.send('Obtener elementos')
--- src/routes/element.routes.js
+++ src/routes/element.routes.js
@@ -1,1 +1,11 @@
 //TAREA ASIGNADA A MARCELO CONDORI
+import {Router} from 'express'
+
+import {getElement,createElement,updateElement,deleteElement} from "../controllers/element.controllers.js"
+
+const router = Router()
+router.get('/element',getElement)
+router.post('/element',createElement)
+router.put('/element',updateElement)
+router.delete('/element',deleteElement)
+export default router
