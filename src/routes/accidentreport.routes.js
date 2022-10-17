b2a94f3e367a229767449bb9da5f114b0a5183e1
charly607356 <jac5000526@est.univalle.edu>
2022-10-16 20:59:46 -0400

    Acident-Report-Charly

--- src/routes/accidentreport.routes.js
+++ src/routes/accidentreport.routes.js
@@ -1,1 +1,26 @@
 //TAREA ASIGNADA A CHARLY JIMENEZ
+import { Router } from 'express'
+import {
+    getaccidentreport,
+    createaccidentreport,
+    updateaccidentreport,
+    deleteaccidentreport, 
+    getaccidentreports, 
+} from '../controllers/accidentreport.controllers.js';
+
+const router = Router ()
+
+router.get('/report', getaccidentreport);
+
+router.get('/report/:id', getaccidentreports);
+
+
+router.post('/report', createaccidentreport);
+
+
+router.patch('/report/:id', updateaccidentreport);
+
+router.delete('/report/:id', deleteaccidentreport);
+
+
+export default router;
