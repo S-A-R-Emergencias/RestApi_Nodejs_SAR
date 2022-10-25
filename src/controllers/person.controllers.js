bce2e51a620ca4c6c9bce4accd4dec619742bf69
FernandoTFW <fer.aparicio1811@gmail.com>
2022-10-25 16:20:44 -0400

    Se ha agregado la base de datos alternativa de railway

--- src/controllers/person.controllers.js
+++ src/controllers/person.controllers.js
@@ -27,12 +27,27 @@ export const getPerson = async (req,res) => {
     
 }
 
+export const loginPerson = async (req,res) => {
+
+    try{
+        const [rows] = await pool.promise().query('SELECT * FROM person WHERE email = ? AND password = ? AND status = 1' ,[req.params.email,req.params.password])
+        if(rows.length <= 0 ) return res.status(404).json({
+            message: 'Person not found'
+        })
+        res.json(rows[0])
+    }catch(error){
+        return res.status(500).json({message: 'Something goes wrong'})
+    }
+    
+}
+
 export const createPerson = async (req,res) => {
     const {name,lastName,secondLastName,ci,address,birthDate,email,telephone,password,role} = req.body
     try{
         const [rows] = await pool.promise().query(
             "INSERT INTO person(name,lastName,secondLastName,ci,address,birthDate,email,telephone,password,role) VALUES (?,?,?,?,?,?,?,?,?,?)",
             [name,lastName,secondLastName,ci,address,birthDate,email,telephone,password,role])
+            
         res.send({rows})
     }catch(error){
         return res.status(500).json({message: 'Something goes wrong'})
@@ -43,8 +58,8 @@ export const createPerson = async (req,res) => {
 export const updatePerson = async (req,res) => {
     const {id} = req.params
     const {name,lastName,secondLastName,ci,address,birthDate,email,telephone,password,role} = req.body
-
     try{
+        console.log(req.body)
         const [result] = await pool.promise().query(
             'UPDATE person SET name = IFNULL(?,name), lastName = IFNULL(?,lastName), secondLastName = IFNULL(?,secondLastName), ci = IFNULL(?,ci), address = IFNULL(?,address), birthDate = IFNULL(?,birthDate), email = IFNULL(?,email), telephone = IFNULL(?,telephone) , password =  IFNULL(?,password),role = IFNULL(?,role),  lastUpdate = CURRENT_TIMESTAMP  WHERE id = ?' ,
             [name,lastName,secondLastName,ci,address,birthDate,email,telephone,password,role,id])
